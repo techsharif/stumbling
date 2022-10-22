78363c34eda6ddc5fa82443ad2fd9e5896d7dc43
techsharif <sharif.cse.hstu@gmail.com>
2022-10-22 15:20:57 +0600

    solve problem 2

--- src/main/java/leetcode/_3/Solution.java
+++ src/main/java/leetcode/_3/Solution.java
@@ -1,7 +1,27 @@
 package leetcode._3;
 
+
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        return 0;
+        Set<Character> set = new HashSet<>();
+        int mx = 0;
+        int start = 0;
+        int end = 0;
+
+        while (end < s.length()) {
+            if (set.contains(s.charAt(end))) {
+                set.remove(s.charAt(start));
+                start += 1;
+            } else {
+                set.add(s.charAt(end));
+                end += 1;
+                mx = Math.max(mx, end - start);
+            }
+        }
+
+        return mx;
     }
 }

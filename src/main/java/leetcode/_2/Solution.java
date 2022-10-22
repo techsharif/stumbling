d922ae92723f556d8f372f1c8168413c3d885b14
techsharif <sharif.cse.hstu@gmail.com>
2022-10-22 13:19:12 +0600

    Solved problem 2

--- src/main/java/leetcode/_2/Solution.java
+++ src/main/java/leetcode/_2/Solution.java
@@ -1,8 +1,38 @@
 package leetcode._2;
 
 class Solution {
+
+    public void addTwoNumbers(ListNode l1, ListNode l2, int carry, ListNode result) {
+        if (l1 == null && l2 == null && carry == 0) return;
+
+        int l1Val;
+        int l2Val;
+
+        if (l1 == null) {
+            l1Val = 0;
+        } else {
+            l1Val = l1.val;
+            l1 = l1.next;
+        }
+
+        if (l2 == null) {
+            l2Val = 0;
+        } else {
+            l2Val = l2.val;
+            l2 = l2.next;
+        }
+
+        result.next = new ListNode((l1Val + l2Val + carry) % 10);
+        carry = (l1Val + l2Val + carry) / 10;
+        addTwoNumbers(l1, l2, carry, result.next);
+    }
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+        ListNode result = new ListNode((l1.val + l2.val) % 10);
+        int carry = (l1.val + l2.val) / 10;
+        addTwoNumbers(l1.next, l2.next, carry, result);
+
+        return result;
     }
 
 }
